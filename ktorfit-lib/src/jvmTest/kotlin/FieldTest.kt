817d2af07634cbef9c247cd0e12b6f0db4a6c68a
Jens Klingenberg <mail@jensklingenberg.de>
2022-04-20 23:13:40 +0200

    improve testing

---
+++ ktorfit-lib/src/jvmTest/kotlin/FieldTest.kt
@@ -0,0 +1,43 @@
+import de.jensklingenberg.ktorfit.Ktorfit
+import de.jensklingenberg.ktorfit.internal.FieldData
+import de.jensklingenberg.ktorfit.internal.FieldType
+import de.jensklingenberg.ktorfit.internal.KtorfitClient
+import de.jensklingenberg.ktorfit.internal.RequestData
+import io.ktor.client.*
+import io.ktor.client.request.*
+import io.ktor.client.request.forms.*
+import kotlinx.coroutines.runBlocking
+import org.junit.Assert.assertTrue
+import org.junit.Test
+
+class FieldTest {
+
+    @Test
+    fun testFieldWithString() {
+
+        val testKey = "foo"
+        val testValue = "bar fizz"
+        val encodedTestValue = "bar%20fizz"
+
+        val engine = object : TestEngine() {
+            override fun getRequestData(data: HttpRequestData) {
+                assertTrue( (data.body as FormDataContent).formData[testKey] == encodedTestValue)
+            }
+        }
+
+        val ktorfit = Ktorfit("www.test.de/", HttpClient(engine))
+        runBlocking {
+            val requestData = RequestData(
+                method = "GET",
+                relativeUrl = "",
+                qualifiedRawTypeName = "kotlin.String",
+                fields = listOf(FieldData(false, testKey, testValue, FieldType.FIELD)),
+            )
+            KtorfitClient(ktorfit).suspendRequest<String, String>(requestData)
+        }
+    }
+
+
+}
+
+
