178ea74fe178b53522d6b90b08dd51d08f32a8fc
Jens Klingenberg <mail@jensklingenberg.de>
2022-07-10 18:48:31 +0200

    update dependencies

--- example/MultiplatformExample/build.gradle.kts
+++ example/MultiplatformExample/build.gradle.kts
@@ -6,7 +6,7 @@ buildscript {
     }
     dependencies {
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.20")
-        classpath("com.android.tools.build:gradle:7.3.0-alpha09")
+        classpath("com.android.tools.build:gradle:7.4.0-alpha08")
         classpath("org.jetbrains.kotlin:kotlin-serialization:1.6.21")
 
     }
--- ktorfit-ksp/build.gradle.kts
+++ ktorfit-ksp/build.gradle.kts
@@ -20,9 +20,9 @@ version = "1.0.0-beta07"
 dependencies {
     implementation("com.google.devtools.ksp:symbol-processing-api:$kspVersion")
     testImplementation("junit:junit:4.13.2")
-    testImplementation("com.github.tschuchortdev:kotlin-compile-testing-ksp:1.4.8")
-    implementation("com.squareup:kotlinpoet:1.11.0")
-    implementation("com.squareup:kotlinpoet-ksp:1.11.0")
+    testImplementation("com.github.tschuchortdev:kotlin-compile-testing-ksp:1.4.9")
+    implementation("com.squareup:kotlinpoet:1.12.0")
+    implementation("com.squareup:kotlinpoet-ksp:1.12.0")
     testImplementation("com.google.truth:truth:1.1.3")
     compileOnly ("com.google.auto.service:auto-service:1.0.1")
     kapt ("com.google.auto.service:auto-service:1.0.1")
