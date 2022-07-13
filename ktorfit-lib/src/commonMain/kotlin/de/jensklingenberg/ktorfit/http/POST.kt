ce4d4fbd3801d2b14481c4908378afcd7caf002f
Jens Klingenberg <mail@jensklingenberg.de>
2022-07-13 11:51:20 +0200

    Beta09 (#17)
    * move annotations
    
    * #15: fix multiple encoding of query parameters
    
    * update version number

--- ktorfit-lib/src/commonMain/kotlin/de/jensklingenberg/ktorfit/http/POST.kt
+++
@@ -1,7 +0,0 @@
-package de.jensklingenberg.ktorfit.http
-
-/** Make a POST request.
- * @param value relative url path, if empty, you need to have a parameter with [Url]
- * */
-@Target(AnnotationTarget.FUNCTION)
-annotation class POST(val value: String)
