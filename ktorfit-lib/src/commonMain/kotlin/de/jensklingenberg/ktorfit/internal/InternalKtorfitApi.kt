2c266321f2d4edc9f1e54029ed95ad5a1e462f59
Jens Klingenberg <mail@jensklingenberg.de>
2022-09-07 21:45:47 +0200

    feat: add InternalKtorfitApi annotation

--- ktorfit-lib/src/commonMain/kotlin/de/jensklingenberg/ktorfit/internal/InternalKtorfitApi.kt
+++ ktorfit-lib/src/commonMain/kotlin/de/jensklingenberg/ktorfit/internal/InternalKtorfitApi.kt
@@ -2,6 +2,6 @@ package de.jensklingenberg.ktorfit.internal
 
 @RequiresOptIn(
     level = RequiresOptIn.Level.WARNING,
-    message = "This is internal API for my Ktorfit, please don't rely on it."
+    message = "This is internal API for Ktorfit, please don't rely on it."
 )
 public annotation class InternalKtorfitApi
