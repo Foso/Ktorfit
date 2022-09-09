853b120df3f9710d32b1f3242205648f1c3867f8
Jens Klingenberg <mail@jensklingenberg.de>
2022-09-09 20:37:44 +0200

    refactor(KSP): simplify code generation

--- ktorfit-ksp/src/main/kotlin/de/jensklingenberg/ktorfit/requestData/GetPathsText.kt
+++ ktorfit-ksp/src/main/kotlin/de/jensklingenberg/ktorfit/requestData/GetPathsText.kt
@@ -5,17 +5,18 @@ import de.jensklingenberg.ktorfit.model.ParameterData
 import de.jensklingenberg.ktorfit.model.annotations.Path
 import de.jensklingenberg.ktorfit.utils.surroundIfNotEmpty
 
+/**
+ * Source for the "paths" argument of [de.jensklingenberg.ktorfit.RequestData]
+ */
 fun getPathsText(params: List<ParameterData>): String {
-    var pathData = ""
-
-    pathData += params.filter { it.hasAnnotation<Path>() }.map { myParam ->
+    val pathData = params.filter { it.hasAnnotation<Path>() }.map { myParam ->
         val paramName = myParam.name
         val pathAnnotation = myParam.findAnnotationOrNull<Path>()
         val pathPath = pathAnnotation?.value ?: ""
         val pathEncoded = pathAnnotation?.encoded ?: false
 
         "${pathDataClass.name}(\"$pathPath\",\"\$$paramName\",$pathEncoded)"
-    }.joinToString { it }.surroundIfNotEmpty("paths = listOf(", ")")
+    }.joinToString { it }
 
-    return pathData
+    return pathData.surroundIfNotEmpty("paths = listOf(", ")")
 }
