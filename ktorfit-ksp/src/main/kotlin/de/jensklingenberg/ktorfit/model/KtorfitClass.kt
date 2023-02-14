5651dc5560b72f500256dba77f5e61d265827d69
Jens Klingenberg <mail@jensklingenberg.de>
2023-02-14 19:21:07 +0100

    Use setClient() as default impl in KtorfitService to reduce code generation

--- ktorfit-ksp/src/main/kotlin/de/jensklingenberg/ktorfit/model/KtorfitClass.kt
+++ ktorfit-ksp/src/main/kotlin/de/jensklingenberg/ktorfit/model/KtorfitClass.kt
@@ -4,10 +4,9 @@ import com.squareup.kotlinpoet.ClassName
 
 data class KtorfitClass(val name: String, val packageName: String, val objectName: String)
 
-val ktorfitClientClass = KtorfitClass("KtorfitClient", "de.jensklingenberg.ktorfit.internal", "client")
+val ktorfitClientClass = KtorfitClass("KtorfitClient", "de.jensklingenberg.ktorfit.internal", "ktorfitClient")
 val ktorfitClass = KtorfitClass("Ktorfit", "de.jensklingenberg.ktorfit", "")
 val requestDataClass = KtorfitClass("RequestData", "de.jensklingenberg.ktorfit.internal", "requestData")
-val pathDataClass = KtorfitClass("PathData", "de.jensklingenberg.ktorfit.internal", "")
 val ktorfitServiceClassName = ClassName("de.jensklingenberg.ktorfit.internal", "KtorfitService")
 
 fun KtorfitClass.toClassName() = ClassName(this.packageName, this.name)
