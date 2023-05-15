0a276c67b9321d2ecdd2a948a67e2a70e5a03be0
Jens Klingenberg <mail@jensklingenberg.de>
2023-05-15 21:26:40 +0200

    Fix documentation

--- ktorfit-lib-common/src/commonMain/kotlin/de/jensklingenberg/ktorfit/internal/RequestData.kt
+++ ktorfit-lib-common/src/commonMain/kotlin/de/jensklingenberg/ktorfit/internal/RequestData.kt
@@ -4,8 +4,6 @@ import io.ktor.client.request.*
 import io.ktor.util.reflect.*
 
 /**
- * @param relativeUrl This is the request relative url path e.g. "posts/1"
- * @param fields list of fields parameters that will be added to the formData Body
  * @param returnTypeData This is the qualifiedName of requested return type
  * It will be used by [ResponseConverter] to check if they support the type
  * Because on JS the qualifiedName reflection does not exist, it is inserted as arguments by the Compiler Plugin
