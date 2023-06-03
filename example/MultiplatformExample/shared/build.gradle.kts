b52085f5cd0348c92fc8356c6399159c079287d0
Jens Klingenberg <mail@jensklingenberg.de>
2023-06-03 09:12:04 +0200

    chore: Update (Ktor 2.3.1, Kotlin Poet 1.14.2)

--- example/MultiplatformExample/shared/build.gradle.kts
+++ example/MultiplatformExample/shared/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 }
 
 version = "1.0"
-val ktorVersion = "2.3.0"
+val ktorVersion = "2.3.1"
 val ktorfitVersion = "1.4.0"
 
 configure<de.jensklingenberg.ktorfit.gradle.KtorfitGradleConfiguration> {
