66b4a9493d8b6e2ea85e1a9744764c6979e84b13
Jens Klingenberg <mail@jensklingenberg.de>
2023-09-06 22:54:51 +0200

    Fix sandbox module

---
+++ sandbox/src/commonMain/kotlin/com/example/model/ExampleApi.kt
@@ -0,0 +1,9 @@
+package com.example.model
+
+import de.jensklingenberg.ktorfit.Response
+import de.jensklingenberg.ktorfit.http.GET
+
+interface ExampleApi {
+    @GET("example.json")
+    suspend fun getUser(): Response<User>
+}
