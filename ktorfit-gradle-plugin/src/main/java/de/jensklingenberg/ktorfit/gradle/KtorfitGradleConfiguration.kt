9dd9da226f62b4a428c61fa5a10a022aa5a6b16e
Jens Klingenberg <mail@jensklingenberg.de>
2023-12-07 19:22:40 +0100

    Remove version from Gradle Plugin

--- ktorfit-gradle-plugin/src/main/java/de/jensklingenberg/ktorfit/gradle/KtorfitGradleConfiguration.kt
+++ ktorfit-gradle-plugin/src/main/java/de/jensklingenberg/ktorfit/gradle/KtorfitGradleConfiguration.kt
@@ -9,8 +9,7 @@ open class KtorfitGradleConfiguration {
     /**
      * version number of the compiler plugin
      */
-    @Deprecated("Update the Gradle plugin instead of updating this version")
-    var version: String = "1.11.0" // remember to bump this version before any release!
+    private var version: String = "1.11.0" // remember to bump this version before any release!
 
     /**
      * used to get debug information from the compiler plugin
