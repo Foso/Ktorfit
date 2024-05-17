2de960b887c72cf46a830e2a9b5c8541bf98037c
Jens Klingenberg <mail@jensklingenberg.de>
2024-05-17 21:21:01 +0200

    Release 2.0.0-beta2

--- example/MultiplatformExample/shared/build.gradle.kts
+++ example/MultiplatformExample/shared/build.gradle.kts
@@ -4,12 +4,12 @@ plugins {
     id("com.android.library")
     id("com.google.devtools.ksp") version "1.9.23-1.0.20"
     id("kotlinx-serialization")
-    id("de.jensklingenberg.ktorfit") version "2.0.0-beta1"
+    id("de.jensklingenberg.ktorfit") version "2.0.0-beta2-SNAPSHOT"
 }
 
 version = "1.0"
 val ktorVersion = "2.3.10"
-val ktorfitVersion = "2.0.0-beta1"
+val ktorfitVersion = "2.0.0-beta2-SNAPSHOT"
 
 kotlin {
     jvmToolchain(8)
