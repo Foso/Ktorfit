2825edc890439bd42ee9d1aae27b51ae4eedfd0a
Jens Klingenberg <mail@jensklingenberg.de>
2024-05-18 22:30:07 +0200

    Release 2.0.0-beta2

--- example/AndroidOnlyExample/app/build.gradle.kts
+++ example/AndroidOnlyExample/app/build.gradle.kts
@@ -65,8 +65,8 @@ dependencies {
     implementation("de.jensklingenberg.ktorfit:ktorfit-converters-flow:$ktorfit")
 
 
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
-    implementation("androidx.activity:activity-compose:1.8.0")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.0")
+    implementation("androidx.activity:activity-compose:1.9.0")
     implementation("androidx.compose.ui:ui:$compose_ui_version")
     implementation("androidx.compose.ui:ui-tooling-preview:$compose_ui_version")
     implementation("androidx.compose.material:material:1.5.2")
