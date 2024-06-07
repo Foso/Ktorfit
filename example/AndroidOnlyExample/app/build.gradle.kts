9e2c2734ba48253dce1a6f214f1c7047865e98cb
Jens Klingenberg <mail@jensklingenberg.de>
2024-06-07 08:23:52 +0200

    Update to KSP 1.0.22

--- example/AndroidOnlyExample/app/build.gradle.kts
+++ example/AndroidOnlyExample/app/build.gradle.kts
@@ -5,7 +5,6 @@ plugins {
     id("org.jetbrains.kotlin.plugin.serialization") version "2.0.0"
     id("de.jensklingenberg.ktorfit") version "2.0.0"
     id("org.jetbrains.kotlin.plugin.compose") version "2.0.0"
-
 }
 
 ktorfit{
