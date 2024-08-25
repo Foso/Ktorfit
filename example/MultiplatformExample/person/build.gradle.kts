8e87e38671b139dd9b9ba6f65f9d4e1f16116b24
Jens Klingenberg <mail@jensklingenberg.de>
2024-08-25 19:08:33 +0200

    Add multi module example

---
+++ example/MultiplatformExample/person/build.gradle.kts
@@ -0,0 +1,72 @@
+plugins {
+    kotlin("multiplatform")
+    id("com.android.library")
+    id("kotlinx-serialization")
+}
+
+version = "1.0"
+val ktorVersion = "2.3.11"
+
+kotlin {
+    jvmToolchain(8)
+    targetHierarchy.default()
+
+    jvm()
+    androidTarget()
+    iosX64()
+    iosArm64()
+    iosSimulatorArm64()
+    macosX64()
+    js(IR) {
+        this.nodejs()
+        binaries.executable()
+    }
+
+    sourceSets {
+        val commonMain by getting {
+            dependencies {
+                // Only needed when you want to use Kotlin Serialization
+                implementation("io.ktor:ktor-client-serialization:$ktorVersion")
+                implementation("io.ktor:ktor-serialization-kotlinx-json:$ktorVersion")
+            }
+        }
+        val commonTest by getting {
+            dependencies {
+                implementation(kotlin("test"))
+            }
+        }
+        val androidMain by getting
+        val jvmMain by getting
+        val jsMain by getting
+        val iosMain by getting
+        val macosX64Main by getting
+    }
+}
+
+android {
+    compileSdk = 34
+    sourceSets["main"].manifest.srcFile("src/androidMain/AndroidManifest.xml")
+    defaultConfig {
+        minSdk = 21
+        targetSdk = 34
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+}
+
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+    kotlinOptions {
+        jvmTarget = "1.8"
+        allWarningsAsErrors = false
+    }
+}
+
+allprojects {
+    repositories {
+        maven {
+            url = uri("https://oss.sonatype.org/content/repositories/snapshots/")
+        }
+    }
+}
