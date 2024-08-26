704b9856ca3293137821b0d7b3ffbc9d4fd16694
Jens Klingenberg <mail@jensklingenberg.de>
2024-08-26 21:10:32 +0200

    Move null check to default converter

--- ktorfit-lib-core/src/commonMain/kotlin/de/jensklingenberg/ktorfit/converter/builtin/DefaultSuspendResponseConverterFactory.kt
+++ ktorfit-lib-core/src/commonMain/kotlin/de/jensklingenberg/ktorfit/converter/builtin/DefaultSuspendResponseConverterFactory.kt
@@ -11,25 +11,27 @@ import io.ktor.client.statement.HttpResponse
  * It is automatically applied last
  */
 internal class DefaultSuspendResponseConverterFactory : Converter.Factory {
-    class DefaultSuspendResponseConverter(val typeData: TypeData) :
-        Converter.SuspendResponseConverter<HttpResponse, Any> {
-        override suspend fun convert(result: KtorfitResult): Any {
-            return when (result) {
+    class DefaultSuspendResponseConverter(
+        val typeData: TypeData
+    ) : Converter.SuspendResponseConverter<HttpResponse, Any?> {
+        override suspend fun convert(result: KtorfitResult): Any? =
+            when (result) {
                 is KtorfitResult.Failure -> {
-                    throw result.throwable
+                    if (typeData.isNullable) {
+                        null
+                    } else {
+                        throw result.throwable
+                    }
                 }
 
                 is KtorfitResult.Success -> {
                     result.response.call.body(typeData.typeInfo)
                 }
             }
-        }
     }
 
     override fun suspendResponseConverter(
         typeData: TypeData,
         ktorfit: Ktorfit,
-    ): Converter.SuspendResponseConverter<HttpResponse, *> {
-        return DefaultSuspendResponseConverter(typeData)
-    }
+    ): Converter.SuspendResponseConverter<HttpResponse, *> = DefaultSuspendResponseConverter(typeData)
 }
