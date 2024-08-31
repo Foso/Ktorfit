9ee3b5e1238c3bc7b35209198a0bcce59ab7ba5f
Jens Klingenberg <mail@jensklingenberg.de>
2024-08-31 21:11:12 +0200

    Fix compilation for single target KMP projects #593

--- example/MultiplatformExample/person/src/commonMain/kotlin/com/example/ktorfittest/Person.kt
+++ example/MultiplatformExample/person/src/commonMain/kotlin/com/example/ktorfittest/Person.kt
@@ -1,4 +1,4 @@
-package com.example.lib
+package com.example.ktorfittest
 
 @kotlinx.serialization.Serializable
 data class Person(
@@ -16,4 +16,3 @@ data class Person(
     val name: String? = null,
     val height: String? = null
 )
-
--- example/MultiplatformExample/shared/src/commonMain/kotlin/com/example/ktorfittest/StarWarsApi.kt
+++ example/MultiplatformExample/shared/src/commonMain/kotlin/com/example/ktorfittest/StarWarsApi.kt
@@ -1,6 +1,5 @@
 package com.example.ktorfittest
 
-import com.example.lib.Person
 import de.jensklingenberg.ktorfit.Call
 import de.jensklingenberg.ktorfit.Response
 import de.jensklingenberg.ktorfit.http.GET
@@ -14,18 +13,29 @@ interface StarWarsApi {
     }
 
     @GET("people/{id}/")
-    suspend fun getPersonByIdResponse(@Path("id") peopleId: Int): Person
+    suspend fun getPersonByIdResponse(
+        @Path("id") peopleId: Int
+    ): Person
 
     @GET("people/{id}/")
-    fun getPeopleByIdFlowResponse(@Path("id") peopleId: Int, @Query("hello") world: String?): Flow<Person>
+    fun getPeopleByIdFlowResponse(
+        @Path("id") peopleId: Int,
+        @Query("hello") world: String?
+    ): Flow<Person>
 
     @GET("people/{id}/")
-    fun getPeopleByIdCallResponse(@Path("id") peopleId: Int): Call<Person>
-
-
-    @GET("people/{id}/")
-    fun queryTest(@Path("id") peopleId: Int, @Query("hello") world: String?): Call<Person>
+    fun getPeopleByIdCallResponse(
+        @Path("id") peopleId: Int
+    ): Call<Person>
 
     @GET("people/{id}/")
-    suspend fun getPersonResponse(@Path("id") personId: Int): Response<Person>
+    fun queryTest(
+        @Path("id") peopleId: Int,
+        @Query("hello") world: String?
+    ): Call<Person>
+
+    @GET("people/{id}/")
+    suspend fun getPersonResponse(
+        @Path("id") personId: Int
+    ): Response<Person>
 }
