e99ac3ff44a0b5b0eb8a50d577839d9d4e94eb0c
Jens Klingenberg <mail@jensklingenberg.de>
2024-09-08 20:31:52 +0200

    Revert unstable class inheritance change #663

--- ktorfit-ksp/src/main/kotlin/de/jensklingenberg/ktorfit/model/FunctionData.kt
+++ ktorfit-ksp/src/main/kotlin/de/jensklingenberg/ktorfit/model/FunctionData.kt
@@ -9,7 +9,16 @@ import de.jensklingenberg.ktorfit.model.annotations.FunctionAnnotation
 import de.jensklingenberg.ktorfit.model.annotations.HttpMethod
 import de.jensklingenberg.ktorfit.model.annotations.HttpMethodAnnotation
 import de.jensklingenberg.ktorfit.model.annotations.Multipart
-import de.jensklingenberg.ktorfit.model.annotations.ParameterAnnotation.*
+import de.jensklingenberg.ktorfit.model.annotations.ParameterAnnotation.Header
+import de.jensklingenberg.ktorfit.model.annotations.ParameterAnnotation.HeaderMap
+import de.jensklingenberg.ktorfit.model.annotations.ParameterAnnotation.Body
+import de.jensklingenberg.ktorfit.model.annotations.ParameterAnnotation.Path
+import de.jensklingenberg.ktorfit.model.annotations.ParameterAnnotation.RequestType
+import de.jensklingenberg.ktorfit.model.annotations.ParameterAnnotation.Field
+import de.jensklingenberg.ktorfit.model.annotations.ParameterAnnotation.Tag
+import de.jensklingenberg.ktorfit.model.annotations.ParameterAnnotation.FieldMap
+import de.jensklingenberg.ktorfit.model.annotations.ParameterAnnotation.Url
+import de.jensklingenberg.ktorfit.model.annotations.ParameterAnnotation.RequestBuilder
 import de.jensklingenberg.ktorfit.poetspec.findTypeName
 import de.jensklingenberg.ktorfit.utils.anyInstance
 import de.jensklingenberg.ktorfit.utils.getFormUrlEncodedAnnotation
