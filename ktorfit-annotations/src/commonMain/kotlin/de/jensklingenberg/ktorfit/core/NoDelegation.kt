1f84f03cb68e5dcccc2bb29c2f28415ddae23ae7
Jens Klingenberg <mail@jensklingenberg.de>
2024-09-10 18:14:26 +0200

    Move NoDelegation annotation

--- ktorfit-annotations/src/commonMain/kotlin/de/jensklingenberg/ktorfit/core/NoDelegation.kt
+++ ktorfit-annotations/src/commonMain/kotlin/de/jensklingenberg/ktorfit/core/NoDelegation.kt
@@ -1,4 +1,4 @@
-package de.jensklingenberg.ktorfit.http
+package de.jensklingenberg.ktorfit.core
 
 /**
  * Indicates that the annotated interface should not be delegated in the generated implementation.
@@ -6,6 +6,9 @@ package de.jensklingenberg.ktorfit.http
  * When an interface is annotated with @NoDelegation, the generated implementation will not use
  * Kotlin delegation for this interface. This is useful when you want to manually implement the
  * methods of the interface or when delegation is not desired for other reasons.
+ *
+ * Example:
+ * interface MyApi : OtherApi, @NoDelegation BaseApi
  */
 @Target(AnnotationTarget.TYPE)
 annotation class NoDelegation
