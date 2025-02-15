a1cdbfd9947a157d5213f5590f1f2720ffd46778
Jens Klingenberg <mail@jensklingenberg.de>
2025-02-15 17:55:05 +0100

    Replace deprecated code

--- ktorfit-lib-core/src/commonMain/kotlin/de/jensklingenberg/ktorfit/TypeInfoExt.kt
+++ ktorfit-lib-core/src/commonMain/kotlin/de/jensklingenberg/ktorfit/TypeInfoExt.kt
@@ -14,5 +14,5 @@ public fun TypeInfo.upperBoundType(index: Int = 0): TypeInfo? {
     val modelKTypeProjection = if (parentType.arguments.isNotEmpty()) parentType.arguments[index] else return null
     val modelKType = modelKTypeProjection.type ?: return null
     val modelClass = (modelKType.classifier as? KClass<*>?) ?: return null
-    return TypeInfo(modelClass, modelKType.platformType, modelKType)
+    return TypeInfo(modelClass, modelKType)
 }
