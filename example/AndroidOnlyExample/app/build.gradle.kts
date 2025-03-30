7547440ab8516fc842bd8f280d3c9be5d050899d
Jens Klingenberg <mail@jensklingenberg.de>
2025-03-30 23:30:53 +0200

    Release 2.5.0 (#838)

--- example/AndroidOnlyExample/app/build.gradle.kts
+++ example/AndroidOnlyExample/app/build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
     id("org.jetbrains.kotlin.android")
     id("com.google.devtools.ksp") version "2.1.10-1.0.31"
     id("org.jetbrains.kotlin.plugin.serialization") version "2.1.0"
-    id("de.jensklingenberg.ktorfit") version "2.4.0"
+    id("de.jensklingenberg.ktorfit") version "2.5.0"
     id("org.jetbrains.kotlin.plugin.compose") version "2.1.10"
 }
 
@@ -52,8 +52,8 @@ tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach 
     }
 }
 
-val ktorfitVersion = "2.4.0"
-val ktor = "3.1.1"
+val ktorfitVersion = "2.5.0"
+val ktor = "3.1.2"
 val compose_ui_version = "1.7.8"
 dependencies {
     implementation("de.jensklingenberg.ktorfit:ktorfit-lib:$ktorfitVersion")
