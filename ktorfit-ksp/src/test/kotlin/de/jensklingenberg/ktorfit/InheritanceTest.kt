98f0b974ba1079956d609b3be24e2ff0cf6da29b
Jens Klingenberg <mail@jensklingenberg>
2025-12-05 21:32:34 +0100

    Refactor code generation

--- ktorfit-ksp/src/test/kotlin/de/jensklingenberg/ktorfit/InheritanceTest.kt
+++ ktorfit-ksp/src/test/kotlin/de/jensklingenberg/ktorfit/InheritanceTest.kt
@@ -35,10 +35,18 @@ interface TestService : SuperTestService {
 
         val expectedGeneratedCode =
             buildString {
+                /**
+                 * public class _TestServiceImpl(
+                 *   private val _baseUrl: String,
+                 *   private val _helper: KtorfitConverterHelper,
+                 * ) : TestService,
+                 *     SuperTestService by com.example.api._SuperTestServiceImpl( _baseUrl,_helper) {
+                 */
                 append("public class _TestServiceImpl(\n")
-                append("  private val _ktorfit: Ktorfit,\n")
+                append("  private val _baseUrl: String,\n")
+                append("  private val _helper: KtorfitConverterHelper,\n")
                 append(") : TestService,\n")
-                append("    SuperTestService by com.example.api._SuperTestServiceImpl(_ktorfit) {")
+                append("    SuperTestService by com.example.api._SuperTestServiceImpl( _baseUrl,_helper) {\n")
             }
 
         val compilation = getCompilation(listOf(source))
@@ -88,8 +96,9 @@ interface TestService : @NoDelegation SuperTestService1, @NoDelegation SuperTest
 
         val expectedHeadersArgumentText =
             "public class _TestServiceImpl(\n" +
-                "  private val _ktorfit: Ktorfit,\n" +
-                ") : TestService {"
+                    "  private val _baseUrl: String,\n" +
+                    "  private val _helper: KtorfitConverterHelper,\n" +
+                    ") : TestService {"
 
         val compilation = getCompilation(listOf(source))
         val result = compilation.compile()
