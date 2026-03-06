bf88acc7870384774398dde1f68197dd8ffa9844
Anthony Legay <anthony.legay@decathlon.com>
2026-03-06 17:05:28 +0100

    Ktlint indentation fix

--- ktorfit-ksp/src/main/kotlin/de/jensklingenberg/ktorfit/reqBuilderExtension/AttributesCodeGenerator.kt
+++ ktorfit-ksp/src/main/kotlin/de/jensklingenberg/ktorfit/reqBuilderExtension/AttributesCodeGenerator.kt
@@ -9,39 +9,38 @@ import de.jensklingenberg.ktorfit.utils.toClassName
 fun getAttributesCode(
     functionData: FunctionData
 ): String {
+    val templatePath = functionData.httpMethodAnnotation.path
     val parameterAttributes =
         "setAttributes {\n" +
-            "put(AttributeKey(\"${pathTemplateAttributeKey.objectName}\"), \"${functionData.httpMethodAnnotation.path}\")\n" +
-        functionData.parameterDataList
-            .filter { it.hasAnnotation<ParameterAnnotation.Tag>() }
-            .joinToString("\n") {
-                val tag =
-                    it.findAnnotationOrNull<ParameterAnnotation.Tag>()
-                        ?: throw IllegalStateException("Tag annotation not found")
-                if (it.type.parameterType.isMarkedNullable) {
-                    "${it.name}?.let{ put(AttributeKey(\"${tag.value}\"), it) }"
-                } else {
-                    "put(AttributeKey(\"${tag.value}\"), ${it.name})"
-                }
-            } + "}"
-
+            "put(AttributeKey(\"${pathTemplateAttributeKey.objectName}\"), \"${templatePath}\")\n" +
+            functionData.parameterDataList
+                .filter { it.hasAnnotation<ParameterAnnotation.Tag>() }
+                .joinToString("\n") {
+                    val tag =
+                        it.findAnnotationOrNull<ParameterAnnotation.Tag>()
+                            ?: throw IllegalStateException("Tag annotation not found")
+                    if (it.type.parameterType.isMarkedNullable) {
+                        "${it.name}?.let{ put(AttributeKey(\"${tag.value}\"), it) }"
+                    } else {
+                        "put(AttributeKey(\"${tag.value}\"), ${it.name})"
+                    }
+                } + "}"
 
     if (functionData.nonKtorfitAnnotations.isEmpty()) return parameterAttributes
 
     val annotationsAttribute =
-        functionData.nonKtorfitAnnotations.joinToString(
-            separator = ",\n",
-            prefix = "listOf(\n",
-            postfix = ",\n)",
-        ) { annotation ->
-            annotation
-                .members
-                .joinToString {
-                    it.toString().replace("@", "")
-                }
-                .let { "${annotation.toClassName().simpleName}($it)" }
-        }
-            .let { "attributes.put(${annotationsAttributeKey.objectName}, $it)" }
+        functionData.nonKtorfitAnnotations
+            .joinToString(
+                separator = ",\n",
+                prefix = "listOf(\n",
+                postfix = ",\n)",
+            ) { annotation ->
+                annotation
+                    .members
+                    .joinToString {
+                        it.toString().replace("@", "")
+                    }.let { "${annotation.toClassName().simpleName}($it)" }
+            }.let { "attributes.put(${annotationsAttributeKey.objectName}, $it)" }
 
     return if (parameterAttributes.isNotEmpty()) {
         parameterAttributes + "\n" + annotationsAttribute
