29a4ec09b36ed222c51889052b6fbe0f8604b64e
Eliezer Graber <me@eygraber.com>
2026-03-12 07:45:41 -0400

    Don't require signing when publishing to maven local

--- build-logic/src/main/kotlin/ktorfit.publishing.gradle.kts
+++ build-logic/src/main/kotlin/ktorfit.publishing.gradle.kts
@@ -1,3 +1,5 @@
+import org.gradle.plugins.signing.Sign
+
 plugins {
     id("com.vanniktech.maven.publish")
 }
@@ -5,4 +7,13 @@ plugins {
 mavenPublishing {
     publishToMavenCentral(automaticRelease = true)
     signAllPublications()
+}
+
+tasks.withType<Sign>().configureEach {
+    onlyIf("not publishing to mavenLocal") {
+        !gradle.taskGraph.allTasks.any {
+            it.name.contains("publishToMavenLocal", ignoreCase = true) ||
+                    it.name.contains("publishAppleToMavenLocal", ignoreCase = true)
+        }
+    }
 }
